d20bcad489a01887deda58459fb1264b495853a0
bilel-bouchoukh <bilel.bouchoukh@laplateforme.io>
2025-09-10 15:03:35 +0200

    ajout du model faune

---
+++ api/App/Models/FauneModel.php
@@ -0,0 +1,44 @@
+<?php 
+
+namespace App\Models;
+
+use Core\Database;
+
+final class FauneModel extends Database {
+
+    public function __construct()
+    {
+        parent::__construct('faune');
+    }
+
+    public function getFaune(int $id){
+        $query = 
+            "
+            SELECT * FROM faune
+            WHERE id = :id
+            LIMIT 1
+            ";
+        
+        parent::sqlQuery($query, ['id' => $id]);
+        return $this->stmt->fetch() ?? [];
+    }
+
+    public function getAllFaune($filters) {
+        $query = 
+        "
+        SELECT * FROM faune
+        ".parent::buildClauses($filters);
+    }
+
+    public function createFaune(array $data){
+        return $this->create($data);
+    }
+
+    public function deleteFaune(int $id) {
+        return $this->delete($id);
+    }
+
+    public function updateFaune(int $id, array $data){
+        return $this->update($id, $data);
+    }
+}
