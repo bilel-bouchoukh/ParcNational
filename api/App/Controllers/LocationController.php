75ba2d2a3fa08e6a5d66dd2a196ba298f1b9070f
bilel-bouchoukh <bilel.bouchoukh@laplateforme.io>
2025-09-10 10:22:50 +0200

    finalisation du controller car incomplet

--- api/App/Controllers/LocationController.php
+++ api/App/Controllers/LocationController.php
@@ -20,4 +20,36 @@ final class LocationController{
         }
     }
 
+    public function save(array $data){
+        $results = $this->locationModel->createLocation($data);
+        if($results){
+            json_encode([
+                "success" => true,
+                "Les données insérer" => $results
+            ]);
+        }else{
+            json_encode("Erreur lors de la créations des données");
+        }
+    }
+
+    public function edit(int $id, $data) {
+        $results = $this->locationModel->updateLocation($id, $data);
+        if($results){
+            json_encode([
+                "success" => true,
+                "Les données mise à jour" => $results
+            ]);
+        }
+    }
+
+    public function destroy(int $id) {
+        $results = $this->locationModel->deleteLocation($id);
+        if($results){
+            json_encode([
+                "success" => true,
+                "Les données supprimer" => $results
+        ]);
+        }
+    }
+
 }
