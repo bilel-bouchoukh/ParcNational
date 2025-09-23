3952fcb170e9e60b4f85dffb43392bb35d4f15e2
bilel-bouchoukh <bilel.bouchoukh@laplateforme.io>
2025-09-23 15:10:27 +0200

    création du controller

---
+++ api/App/Controllers/TypeLocationController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\TypeLocationModel;
+
+final class TypeLocationController{
+    private TypeLocationModel $typeLocationModel;
+
+    public function __construct()
+    {
+        $this->typeLocationModel = new TypeLocationModel();
+    }
+
+    public function index(array $filters) {
+        $results = $this->typeLocationModel->getAllTypeLocation($filters);
+        if($results) {
+            json_encode([
+                "success" => true,
+                "data" => $results
+            ]);
+        } else {
+            json_encode("Erreur lors de la récupération");
+        }
+    } 
+}
